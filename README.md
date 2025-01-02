8061ef52f9c15eb5dbb2d3bcb35f1f091f08558f
Anton Kudryashov <driver_90@list.ru>
2025-01-02 14:42:32 +0700

    Add Allure

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 [![Java CI with Gradle](https://github.com/GigirKudr/patternsHome1/actions/workflows/gradle-publish.yml/badge.svg)](https://github.com/GigirKudr/patternsHome1/actions/workflows/gradle-publish.yml)
+
+
--- src/test/java/ru/netology/delivery/test/CardTest.java
+++ src/test/java/ru/netology/delivery/test/CardTest.java
@@ -2,10 +2,10 @@ package ru.netology.delivery.test;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.bouncycastle.util.encoders.HexTranslator;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
 
@@ -17,10 +17,23 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardTest {
+
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll(){
+        SelenideLogger.removeListener("allure");
+    }
+
     @BeforeEach
     void setup() {
         open("http://localhost:9999");
     }
+
 
     @Test
     @DisplayName("Date Planer")
